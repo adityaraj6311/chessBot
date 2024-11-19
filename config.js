fc05bd9066eee9469e87cf1f47348e5dbd3cba80
Aditya <adityaraj6313@gmail.com>
2024-11-19 22:24:29 +0530

    config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 import "https://deno.land/std@0.204.0/dotenv/load.ts";
 
 export default {
-    BOT_TOKEN: Deno.env.get("BOT_TOKEN"),
-    MONGO_URI: Deno.env.get("MONGO_URI"),
+    BOT_TOKEN: "7637053751:AAGpZuHVSs5NKnUIASlvMyXQuIHxvY7ZOFQ",
+    MONGO_URI: "mongodb+srv://gojo:gojo@cluster0.vx6fdn0.mongodb.net",
     DB_NAME: "chessBot",
     DEFAULT_ICON: "https://files.catbox.moe/djc10c.png"
 }
